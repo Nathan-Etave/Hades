2183c17e179ab7d1ab79e01b4f1ef4fed8e8361e
ArthurVILLETTE <arthur.villette@etu.univ-orleans.fr>
2023-10-24 09:43:51 +0200

    feat/ajout des tables avec leur suppression et leur clear
    Co-authored-by: RomainMechain <RomainMechain@users.noreply.github.com>
    Co-authored-by: LouisLebeaupin18 <LouisL18@users.noreply.github.com>
    Co-authored-by: Nathan Etave <Nathan-Etave@users.noreply.github.com>

---
+++ app/bd/create_tables.sql
@@ -0,0 +1,99 @@
+create table ETAT_FICHIER(
+    idEtatFichier int,
+    nomEtatFichier varchar(255),
+    descriptionEtatFichier varchar(255),
+    primary key (idEtatFichier)
+)ENGINE=InnoDB;
+
+create table CATEGORIE(
+    idCategorie int,
+    nomCategorie varchar(255),
+    primary key (idCategorie) 
+)ENGINE=InnoDB;
+
+create table SOUS_CATEGORIE(
+    idSousCategorie int,
+    nomSousCategorie varchar(255),
+    idCategorie int,
+    primary key (idSousCategorie),
+    constraint FKsous_categorie_categorie foreign key (idCategorie) references CATEGORIE(idCategorie)
+
+)ENGINE=InnoDB;
+
+create table FICHIER(
+    idFichier int,
+    nomFichier varchar(255),
+    leFichier longblob,
+    extensionfichier varchar(255),
+    idSousCategorie int,
+    primary key (idFichier),
+    constraint FKfichier_sousCategorie foreign key (idSousCategorie) references SOUS_CATEGORIE(idSousCategorie)
+)ENGINE=InnoDB;
+
+create table EST_ETAT(
+    idFichier int,
+    idEtatFichier int,
+    primary key (idFichier, idEtatFichier),
+    constraint FKest_etat_fichier foreign key (idFichier) references FICHIER(idFichier),
+    constraint FKest_etat_etat foreign key (idEtatFichier) references ETAT_FICHIER(idEtatFichier)
+)ENGINE=InnoDB;
+
+create table ROLE_POMPIER(
+    idRole int,
+    nomRole varchar(255),
+    descriptionRole varchar(255),
+    primary key (idRole)
+)ENGINE=InnoDB;
+
+create table POMPIER(
+    idPompier int,
+    nomPompier varchar(255),
+    prenomPompier varchar(255),
+    emailPompier varchar(255),
+    mdpPompier varchar(255),
+    photoPompier int,
+    primary key (idPompier)
+)ENGINE=InnoDB;
+
+create table DATE(
+    idDate int,
+    laDate date,
+    primary key (idDate)
+)ENGINE=InnoDB;
+
+create table A_CONSULTE(
+    idFichier int,
+    idPompier int,
+    idDate int,
+    primary key (idFichier, idPompier,idDate),
+    constraint FKa_consulte_fichier foreign key (idFichier) references FICHIER(idFichier),
+    constraint FKa_consulte_pompier foreign key (idPompier) references POMPIER(idPompier),
+    constraint FKa_consulte_date foreign key (idDate) references DATE(idDate)
+)ENGINE=InnoDB;
+
+create table FAVORI(
+    idFichier int,
+    idPompier int,
+    primary key (idFichier, idPompier),
+    constraint FKfavori_pompier foreign key (idPompier) references POMPIER(idPompier),
+    constraint FKfavori_fichier foreign key (idFichier) references FICHIER(idFichier)
+    
+)ENGINE=InnoDB;
+
+create table SIGNALEMENT(
+    idFichier int,
+    idPompier int,
+    descriptionSignalement varchar(255),
+    primary key (idFichier, idPompier),
+    constraint FKsignalement_fichier foreign key (idFichier) references FICHIER(idFichier),
+    constraint FKsignalement_pompier foreign key (idPompier) references POMPIER(idPompier)
+)ENGINE=InnoDB;
+
+
+
+
+
+
+
+
+
