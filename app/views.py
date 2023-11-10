3377b58543a259ac7ac27267264b276c9aca2250
Nathan-Etave <nathan.etave@gmail.com>
2023-11-10 08:44:04 +0100

    Feature/Ajout des premières routes

---
+++ app/views.py
@@ -0,0 +1,121 @@
+from app import app
+from flask import render_template, request, redirect, url_for, make_response, send_file
+from flask_login import login_required, login_user, logout_user, current_user
+from flask_bcrypt import generate_password_hash, check_password_hash
+from io import BytesIO
+from app.requests import get_root_categories, get_user_by_id, get_user_by_email,get_user_favourites_file, get_file_by_id, user_has_notifications, add_to_user_favourites, remove_from_user_favourites, add_administrator_signalement
+from app.forms import LoginForm
+from app import login_manager
+
+@app.after_request
+def add_cookie(response):
+    if not request.cookies.get('multiview_list'):
+        response.set_cookie('multiview_list', ';')
+    if not request.cookies.get('favourites_list'):
+        response.set_cookie('favourites_list', ';')
+    else:
+        favourites_list = get_user_favourites_file(current_user.get_id())
+        favourites_list = ''.join([f';{f.idFichier}' for f in favourites_list]) + ';'
+        response.set_cookie('favourites_list', favourites_list)
+    return response
+
+@login_manager.user_loader
+def load_user(user_id):
+    return get_user_by_id(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    user = get_user_by_email(form.mail.data)
+    if form.validate_on_submit():
+        if user:
+            if check_password_hash(user.mdpPompier, form.mdp.data):
+                login_user(user, remember=True)
+                return redirect(url_for('home'))
+    return render_template('connexion.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html', nom_page='Accueil', liste_categories=get_root_categories(), liste_fichiers=get_user_favourites_file(current_user.get_id()), notification_enabled=user_has_notifications(current_user.get_id()))
+
+@app.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        search_term = request.form['search']
+        favourites_sort = request.form.get('favoris', False)
+        recent_sort = request.form.get('recent', False)
+        advanced_sort = request.form.get('advanced_search', False)
+        categorie_id = request.form.get('categorie', '')
+        return redirect(url_for('search', search=search_term, categorie=categorie_id,favoris=favourites_sort, recent=recent_sort, advanced_search=advanced_sort))
+    else:
+        search_term = request.args.get('search', type=str, default='')
+        favourites_sort = request.args.get('favoris', type=bool, default='')
+        recent_sort = request.args.get('recent', type=bool, default='')
+        advanced_sort = request.args.get('advanced_search', type=bool, default='')
+        categorie_id = request.args.get('categorie', type=int, default='')
+        return render_template('search.html', nom_page=search_term, categorie=get_root_categories()[0], liste_fichiers=get_user_favourites_file(current_user.get_id()), notification_enabled=user_has_notifications(current_user.get_id()))
+
+@app.route('/file')
+@login_required
+def file():
+    return render_template('file.html' , nom_page='Consultation de fichier', fichier=get_file_by_id(request.args.get('id_fichier', type=int, default='')), notification_enabled=user_has_notifications(current_user.get_id()))
+
+@app.route('/add_to_multiview', methods=['POST'])
+@login_required
+def add_to_multiview():
+    multiview_list = request.cookies.get('multiview_list')
+    file_id = request.get_json()['file_id']
+    if f';{file_id};' not in multiview_list:
+        multiview_list += f'{file_id};'
+    response = app.make_response(redirect(url_for('login')))
+    response.set_cookie('multiview_list', multiview_list)
+    return response
+
+@app.route('/remove_from_multiview', methods=['POST'])
+@login_required
+def remove_from_multiview():
+    multiview_list = request.cookies.get('multiview_list')
+    file_id = request.get_json()['file_id']
+    multiview_list = multiview_list.replace(f';{file_id};', ';')
+    response = app.make_response(redirect(url_for('login')))
+    response.set_cookie('multiview_list', multiview_list)
+    return response
+
+@app.route('/add_to_favourites', methods=['POST'])
+@login_required
+def add_to_favourites():
+    file_id = request.get_json()['file_id']
+    add_to_user_favourites(current_user.get_id(), file_id)
+    return redirect(url_for('login'))
+
+@app.route('/remove_from_favourites', methods=['POST'])
+@login_required
+def remove_from_favourites():
+    file_id = request.get_json()['file_id']
+    remove_from_user_favourites(current_user.get_id(), file_id)
+    return redirect(url_for('login'))
+
+@app.route('/report', methods=['POST'])
+@login_required
+def report():
+    file_id = request.get_json()['file_id']
+    description = request.get_json()['reason']
+    add_administrator_signalement(file_id, current_user.get_id(), description)
+    return redirect(url_for('login'))
+
+
+@app.route('/download_file', methods=['POST'])
+@login_required
+def download_file():
+    file_id = request.get_json()['file_id']
+    file_object = get_file_by_id(file_id)
+    file_data = BytesIO(file_object.data)
+    file_data.seek(0)
+    return send_file(path_or_file=file_data, download_name=file_object.nomFichier, as_attachment=True)
