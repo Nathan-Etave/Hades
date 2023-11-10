942550b5ebaeaf0036318b0ed96b95a742274656
Nathan-Etave <nathan.etave@gmail.com>
2023-11-10 17:22:26 +0100

    Feature/Ajout du form d'édition pour user
    Co-authored-by: LouisLebeaupin18 <LouisL18@users.noreply.github.com>
    Co-authored-by: ArthurVillette <ArthurVillette@users.noreply.github.com>
    Co-authored-by: RomainMechain <RomainMechain@users.noreply.github.com>
    Co-authored-by: Norikokonut <Norikokonut@users.noreply.github.com>

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField, FileField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     mail = StringField(validators=[DataRequired()], render_kw={"placeholder": "Email"})
     mdp = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Mot de passe"})
     submit = SubmitField('Se connecter')
+    
+class EditUserForm(FlaskForm):
+    id = HiddenField('id')
+    nom = StringField(validators=[DataRequired()], render_kw={"placeholder": "Nom"})
+    prenom = StringField(validators=[DataRequired()], render_kw={"placeholder": "Prénom"})
+    mail = StringField(validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    mdp = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Mot de passe"})
+    photo = FileField()
+    telephone = StringField(validators=[DataRequired()], render_kw={"placeholder": "Téléphone"})
+    submit = SubmitField('Modifier')
