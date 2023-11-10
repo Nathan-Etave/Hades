995a8bcdc62193a4a7210e0481788a4aabb3d97c
Nathan-Etave <nathan.etave@gmail.com>
2023-11-10 08:41:33 +0100

    MAJ/Ajout de flask_login (+import requests/forms)

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ""
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.normpath(os.path.join(os.path.dirname(__file__), 'database/app.db'))
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from app import views
 from app import models
 from app import database
+from app import requests
+from app import forms
