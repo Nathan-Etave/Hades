b60e4b4aece4c10f8083eb38d68f3c4ae05d8aed
Nathan-Etave <nathan.etave@gmail.com>
2023-12-18 15:25:54 +0100

    Feature/Ajout des insertions dans la db des consultations de fichiers.

--- app/views.py
+++ app/views.py
@@ -10,8 +10,7 @@ from app.requests import (get_root_categories, get_user_by_id, get_user_by_email
                           get_file_by_id, user_has_notifications, add_to_user_favourites, remove_from_user_favourites,
                           add_administrator_signalement, get_user_notifications, update_user, get_file_order_by_date,
                           update_user_photo, remove_from_user_notification, get_file_by_tag, get_all_files, get_file_history,
-                          get_file_by_categorie, get_file_tags, get_category_tree, add_file_to_database)
-
+                          get_file_by_categorie, get_file_tags, get_category_tree, add_file_to_database, add_consulted_file)
 from app.forms import LoginForm, EditUserForm
 from app import login_manager
 import base64
@@ -120,6 +119,7 @@ def search():
 @app.route('/file')
 @login_required
 def file():
+    add_consulted_file(request.args.get('id_fichier', type=int, default=''), current_user.get_id())
     return render_template('file.html' , nom_page='Consultation de fichier', fichier=get_file_by_id(request.args.get('id_fichier', type=int, default='')), liste_tags=get_file_tags(request.args.get('id_fichier', type=int, default='')), notification_enabled=user_has_notifications(current_user.get_id()))
 
 @app.route('/add_to_multiview', methods=['POST'])
