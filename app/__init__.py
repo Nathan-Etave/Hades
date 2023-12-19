3c43bc7cb496d512fa78f9dcb2a93ec2dccfe2f3
Nathan-Etave <nathan.etave@gmail.com>
2023-12-19 12:39:40 +0100

    Feature/Ajout de la fonctionnalitée de modification des fichiers

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,13 @@ from flask_login import LoginManager
 from flask_dropzone import Dropzone
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'nathan'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.normpath(os.path.join(os.path.dirname(__file__), 'database/app.db'))
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_DEFAULT_MESSAGE'] = 'Glissez-déposez vos fichiers ici<br/>ou<br/>Cliquez ici pour sélectionner vos fichiers'
 app.config['DROPZONE_MAX_FILE_SIZE'] = 2048
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .docx, .xls, .xlsx, .pptx, .txt, .jpg, .jpeg, .png, .gif'
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .docx, .xlsx, .pptx, .txt, .jpg, .jpeg, .png, .gif'
 app.config['UPLOADED_TEMP_DEST'] = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp'))
 db = SQLAlchemy(app)
 dropzone = Dropzone(app)
