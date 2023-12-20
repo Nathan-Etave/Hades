751c2f6bd0858b8462910323c2b5d5734261582c
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-12-20 13:50:09 +0100

    Fix/ correction des conflits

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let advancedSearchForm = document.querySelector(".advanced_search_popup");
     let close = document.querySelector("#close");
     let validerRechercheAvanceButton = document.getElementById("valider_recherche_avance");
+    let downloadButtons = document.querySelectorAll(".bouton_telechargement");
 
     // Charger les sélections précédentes depuis le stockage local
     function loadSelections() {
@@ -23,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById("date").checked = date;
         document.getElementById("extension").value = extension;
     }
-
     advancedSearchButton.addEventListener('click', function() {
         if (advancedSearchForm.style.display === "block") {
             advancedSearchForm.style.display = "none";
@@ -92,4 +92,31 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+    downloadButtons.forEach(function(downloadButton) {
+        let fileId = downloadButton.id;
+        console.log(fileId);
+        downloadButton.addEventListener('click', function() {
+            fetch('download_file', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    file_id: fileId
+                })
+            })
+            .then(response => response.blob())
+            .then(blob => {
+                let downloadLink = document.createElement('a');
+                blob.text().then(function(result) {
+                    let file_data = result;
+                    downloadLink.href = 'data:application/octet-stream;base64,' + file_data;
+                    downloadLink.download = downloadButton.parentElement.querySelector('#bouton_fichier').textContent;
+                    downloadLink.click();
+                    downloadLink.remove();
+                });
+            });
+        });
+    });
 });
+
