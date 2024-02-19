3ed09929150650e336bc6e0afd0b16d342b1566e
Nathan-Etave <nathan.etave@gmail.com>
2024-02-19 15:33:45 +0000

    feat/ Refonte et restructuration pour la v2
    Co-authored-by: Hugo Sainson <Norikokonut@users.noreply.github.com>
    Co-authored-by: RomainMechain <RomainMechain@users.noreply.github.com>

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,23 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_dropzone import Dropzone
+#from flask_dropzone import Dropzone
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'key'
 if not os.path.exists(os.path.normpath(os.path.join(os.path.dirname(__file__), 'database'))):
     os.makedirs(os.path.normpath(os.path.join(os.path.dirname(__file__), 'database')))
-if not os.path.exists(os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp'))):
-    os.makedirs(os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp')))
+# if not os.path.exists(os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp'))):
+#     os.makedirs(os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp')))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.normpath(os.path.join(os.path.dirname(__file__), 'database/app.db'))
-app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
-app.config['DROPZONE_DEFAULT_MESSAGE'] = 'Glissez-déposez vos fichiers ici<br/>ou<br/>Cliquez ici pour sélectionner vos fichiers'
-app.config['DROPZONE_MAX_FILE_SIZE'] = 2048
-app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .docx, .xlsx, .pptx, .txt, .jpg, .jpeg, .png, .gif'
-app.config['UPLOADED_TEMP_DEST'] = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp'))
+#app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
+#app.config['DROPZONE_DEFAULT_MESSAGE'] = 'Glissez-déposez vos fichiers ici<br/>ou<br/>Cliquez ici pour sélectionner vos fichiers'
+#app.config['DROPZONE_MAX_FILE_SIZE'] = 2048
+#app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+#app.config['DROPZONE_ALLOWED_FILE_TYPE'] = '.pdf, .docx, .xlsx, .pptx, .txt, .jpg, .jpeg, .png, .gif'
+#app.config['UPLOADED_TEMP_DEST'] = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/temp'))
 db = SQLAlchemy(app)
-dropzone = Dropzone(app)
+#dropzone = Dropzone(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
