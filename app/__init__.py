072be775ef51148ba384fc753322e8f40d9b4be1
Nathan Etave <nathan.etave@gmail.com>
2024-04-08 16:35:17 +0200

    Mise en place de la base de données.
    Co-authored-by: RomainMechain <RomainMechain@users.noreply.github.com>

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import Config
+from app.extensions import db
+
+from app.models.a_acces import A_ACCES
+from app.models.a_recherche import A_RECHERCHE
+from app.models.a_tag import A_TAG
+from app.models.dossier import DOSSIER
+from app.models.favoris import FAVORIS
+from app.models.fichier import FICHIER
+from app.models.notification import NOTIFICATION
+from app.models.recherche import RECHERCHE
+from app.models.role import ROLE
+from app.models.sous_dossier import SOUS_DOSSIER
+from app.models.tag import TAG
+from app.models.utilisateur import UTILISATEUR
+
+
+def create_app(config_class = Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    return app
