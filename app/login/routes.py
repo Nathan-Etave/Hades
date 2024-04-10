ca35b116b28f78d7176266a628a440ab220b11d7
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-04-10 15:14:14 +0200

    Feat/ Mise en place de la connexion et demande de réactivation.

--- app/login/routes.py
+++ app/login/routes.py
@@ -7,8 +7,12 @@ from flask_login import login_user
 from flask_bcrypt import check_password_hash
 from app.login import bp
 from app.models.utilisateur import UTILISATEUR
+from app.models.notification import NOTIFICATION
 from app.forms.login_form import LoginForm
 from app import login_manager
+from app.extensions import db
+from datetime import datetime
+from flask import jsonify, request
 
 @login_manager.user_loader
 def load_user(user):
@@ -26,16 +30,36 @@ def login():
         if user:
             if user.est_Actif_Utilisateur != 1:
                 if user.id_Role:
-                    flash("Votre compte est désactivé", "danger")
+                    flash("Votre compte est désactivé", "info")
                 else:
                     flash("Votre compte n'est pas encore activé", "danger")
-                return redirect(url_for("login.login"))
+                form.email.data = form.email.data
+                return render_template("login/index.html", form=form)
             if check_password_hash(user.mdp_Utilisateur, form.password.data):
                 login_user(user)
                 flash("Connexion réussie", "success")
                 return redirect(url_for("home.index"))
             flash("Mot de passe incorrect", "danger")
-            return redirect(url_for("login.login"))
+            form.email.data = form.email.data
+            return render_template("login/index.html", form=form)
         flash("Adresse email inconnu", "danger")
-        return redirect(url_for("login.login"))
+        form.email.data = form.email.data
+        return render_template("login/index.html", form=form)
     return render_template("login/index.html", form=form)
+
+
+@bp.route('/notification', methods=['POST'])
+def add_notification() :
+    current_date = datetime.now()
+    type = request.json.get('type')
+    email_user = request.json.get('email_user')
+    id_user = UTILISATEUR.query.filter_by(email_Utilisateur=email_user).first().id_Utilisateur
+    if id_user is None:
+        return jsonify({'error': 'user not found'}), 404
+    notification = NOTIFICATION.query.filter_by(id_Utilisateur=id_user).first()
+    if notification :
+        return jsonify({'error': 'user already have a notification'}), 404
+    notification = NOTIFICATION(datetime_Notification=current_date, type_Notification=type, id_Utilisateur=id_user)
+    db.session.add(notification)
+    db.session.commit()
+    return jsonify(notification.to_dict()), 200
