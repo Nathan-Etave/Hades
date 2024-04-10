e4190f2213602b719c84bad2c36f0a6571ad2e3a
Nathan-Etave <nathan.etave@gmail.com>
2024-04-10 15:24:07 +0200

    feat/ Ajout de la confirmation d'inscription par mail

--- app/static/js/notifications.js
+++ app/static/js/notifications.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let acceptButtons = document.querySelectorAll('.accept-button');
     let rejectButtons = document.querySelectorAll('.reject-button');
     let select = document.querySelectorAll('select');
+
     select.forEach(function (select) {
         select.addEventListener('change', function (event) {
             if (select.value != '') {
@@ -13,69 +14,51 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         select.dispatchEvent(new Event('change'));
     });
-    acceptButtons.forEach(function (button) {
-        button.addEventListener('click', function (event) {
+
+    acceptButtons.forEach(button => {
+        button.addEventListener('click', async function (event) {
             let selectId = event.target.parentElement.parentElement.querySelector('select').value;
             let notificationType = event.target.dataset.notificationType;
             let notificationId = event.target.dataset.notificationId;
             let csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-            let userId = event.target.dataset.userId;
-            if (selectId == '') {
-                alert('Veuillez sélectionner un rôle avant de valider.');
+
+            if (selectId === '') {
+                return alert('Veuillez sélectionner un rôle avant de valider.');
             }
-            else if (userId === '' || isNaN(userId) || !['Inscription', 'Reactivation'].includes(notificationType)) {
+
+            if (!['Inscription', 'Reactivation'].includes(notificationType)) {
+                alert('Erreur lors de la validation.');
+                return window.location.reload();
+            }
+
+            try {
+                let response = await sendRequest(`/notifications/${notificationId}/accept`, 'POST', csrfToken, {
+                    'role_id': selectId
+                });
+
+                if (response.status === 200) {
+                    alert(notificationType === 'Inscription' ? 'Validation effectuée avec succès.' : 'Réactivation effectuée avec succès.');
+                    window.location.reload();
+                }
+                else {
+                    throw new Error();
+                }
+            } catch (error) {
+                console.error(error);
                 alert('Erreur lors de la validation.');
                 window.location.reload();
             }
-            else {
-                if (notificationType === 'Inscription') {
-                    fetch(`/api/utilisateur/${userId}`, {
-                        method: 'PATCH',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'X-CSRFToken': csrfToken
-                        },
-                        body: JSON.stringify({
-                            role: selectId,
-                            actif: true
-                        })
-                    })
-                        .then(response => response.status)
-                        .then(status => {
-                            if (status === 200) {
-                                fetch(`/api/notification/${notificationId}`, {
-                                    method: 'DELETE',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'X-CSRFToken': csrfToken
-                                    }
-                                })
-                                    .then(response => response.status)
-                                    .then(status => {
-                                        if (status === 200) {
-                                            alert('Validation effectuée avec succès.');
-                                            window.location.reload();
-                                        }
-                                        else {
-                                            alert('Erreur lors de la validation.');
-                                            window.location.reload();
-                                        }
-                                    });
-                            }
-                            else if (status === 403 || status === 404) {
-                                alert('Erreur lors de la validation.');
-                                window.location.reload();
-                            }
-                        }
-                        );
-                }
-            }
-        });
-    });
-    rejectButtons.forEach(function (button) {
-        button.addEventListener('click', function (event) {
-            let notificationType = event.target.dataset.notificationType;
-            let userId = event.target.dataset.userId;
         });
     });
 });
+
+async function sendRequest(url, method, csrfToken, body = null) {
+    return await fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: body ? JSON.stringify(body) : null
+    });
+}
