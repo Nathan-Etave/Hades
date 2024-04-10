3469bc218a80cb9876fffd3c62a26677a041dc35
NathanETAVE <nathan.etave@etu.univ-orleans.fr>
2024-04-10 09:09:09 +0200

    fix/ Fix des conflits BD

--- app/models/dossier.py
+++ app/models/dossier.py
@@ -15,6 +15,4 @@ class DOSSIER(db.Model):
     ROLE: Mapped['ROLE'] = relationship('ROLE', secondary='A_ACCES', back_populates='DOSSIER_')
     DOSSIER: Mapped['DOSSIER'] = relationship('DOSSIER', secondary='SOUS_DOSSIER', primaryjoin=lambda: DOSSIER.id_Dossier == SOUS_DOSSIER.c.id_Dossier_Enfant, secondaryjoin=lambda: DOSSIER.id_Dossier == SOUS_DOSSIER.c.id_Dossier_Parent, back_populates='DOSSIER_')
     DOSSIER_: Mapped['DOSSIER'] = relationship('DOSSIER', secondary='SOUS_DOSSIER', primaryjoin=lambda: DOSSIER.id_Dossier == SOUS_DOSSIER.c.id_Dossier_Parent, secondaryjoin=lambda: DOSSIER.id_Dossier == SOUS_DOSSIER.c.id_Dossier_Enfant, back_populates='DOSSIER')
-
-    def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    FICHIER: Mapped[List['FICHIER']] = relationship('FICHIER', uselist=True, back_populates='DOSSIER_')
