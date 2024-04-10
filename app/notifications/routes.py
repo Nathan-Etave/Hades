a7048f4f35a530a5124780ae67fc8dd53b05f352
Nathan Etave <nathan.etave@gmail.com>
2024-04-10 17:08:59 +0200

    feat/ Implémentation des boutons de validation et de rejet

--- app/notifications/routes.py
+++ app/notifications/routes.py
@@ -1,11 +1,12 @@
 from app.notifications import bp
 from app.extensions import db
 from smtplib import SMTPException
-from app.mail.mail import send_registration_confirmation_email, send_registration_rejection_email
+from app.mail.mail import send_registration_confirmation_email, send_registration_rejection_email, send_reactivation_confirmation_email, send_reactivation_rejection_email
+from app.decorators import admin_required
 from app.models.notification import NOTIFICATION
 from app.models.utilisateur import UTILISATEUR
 from app.models.role import ROLE
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, jsonify
 from flask_bcrypt import generate_password_hash
 from flask_login import login_required
 import secrets
@@ -13,6 +14,7 @@ import string
 
 @bp.route('/', methods=['GET'])
 @login_required
+@admin_required
 def notifications():
     notifications = NOTIFICATION.query.all()
     notifications = sorted(notifications, key=lambda x: x.datetime_Notification, reverse=True)
@@ -21,6 +23,7 @@ def notifications():
 
 @bp.route('/<int:id_notification>/accept', methods=['GET', 'POST'])
 @login_required
+@admin_required
 def accept(id_notification):
     notification = NOTIFICATION.query.get(id_notification)
     if notification.type_Notification == 'Inscription':
@@ -35,6 +38,42 @@ def accept(id_notification):
             db.session.commit()
         except (SMTPException, ConnectionError, TimeoutError):
             db.session.rollback()
-            return "Erreur lors de l'envoi de l'email de confirmation", 200
+            return jsonify({'error': 'Erreur lors de l\'envoi du mail. Veuillez réessayer ultérieurement.'}), 500
+    elif notification.type_Notification == 'Reactivation':
+        user = UTILISATEUR.query.get(notification.id_Utilisateur)
+        user.id_Role = request.json.get('role_id')
+        user.est_Actif_Utilisateur = True
+        db.session.delete(notification)
+        try:
+            send_reactivation_confirmation_email(user.email_Utilisateur)
+            db.session.commit()
+        except (SMTPException, ConnectionError, TimeoutError):
+            db.session.rollback()
+            return jsonify({'error': 'Erreur lors de l\'envoi du mail. Veuillez réessayer ultérieurement.'}), 500
+    return render_template('notifications/index.html', is_authenticated=True, is_admin=True), 200
 
+@bp.route('/<int:id_notification>/reject', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def reject(id_notification):
+    notification = NOTIFICATION.query.get(id_notification)
+    if notification.type_Notification == 'Inscription':
+        user = UTILISATEUR.query.get(notification.id_Utilisateur)
+        db.session.delete(notification)
+        try:
+            send_registration_rejection_email(user.email_Utilisateur)
+            db.session.delete(user)
+            db.session.commit()
+        except (SMTPException, ConnectionError, TimeoutError):
+            db.session.rollback()
+            return jsonify({'error': 'Erreur lors de l\'envoi du mail. Veuillez réessayer ultérieurement.'}), 500
+    elif notification.type_Notification == 'Reactivation':
+        user = UTILISATEUR.query.get(notification.id_Utilisateur)
+        db.session.delete(notification)
+        try:
+            send_reactivation_rejection_email(user.email_Utilisateur)
+            db.session.commit()
+        except (SMTPException, ConnectionError, TimeoutError):
+            db.session.rollback()
+            return jsonify({'error': 'Erreur lors de l\'envoi du mail. Veuillez réessayer ultérieurement.'}), 500
     return render_template('notifications/index.html', is_authenticated=True, is_admin=True), 200
--- app/static/js/notifications.js
+++ app/static/js/notifications.js
@@ -2,9 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     let acceptButtons = document.querySelectorAll('.accept-button');
     let rejectButtons = document.querySelectorAll('.reject-button');
     let select = document.querySelectorAll('select');
+    // isFetching implique que les boutons accepter et rejeter soit désactivés pendant la requête, il
+    // faut régler ce problème en utilisant un flash (cf erreur mail) pour afficher un message disant
+    // que telle chose est terminée. (Plus de boutons désactivés (seulement le bouton cliqué et son opposé))
+    // Liste de flash avec un timer de 5sec ? S'affiche un a un ? Ou un seul flash qui s'actualise ?
+    let isFetching = false;
 
     select.forEach(function (select) {
         select.addEventListener('change', function (event) {
+            if (isFetching) {
+                return;
+            }
             if (select.value != '') {
                 select.parentElement.parentElement.querySelector('.accept-button').disabled = false;
             }
@@ -32,21 +40,81 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             try {
+                isFetching = true;
+                acceptButtons.forEach(button => button.disabled = true);
+                rejectButtons.forEach(button => button.disabled = true);
+                event.target.innerHTML = 'Validation en cours...';
                 let response = await sendRequest(`/notifications/${notificationId}/accept`, 'POST', csrfToken, {
                     'role_id': selectId
                 });
-
+            
                 if (response.status === 200) {
+                    isFetching = false;
                     alert(notificationType === 'Inscription' ? 'Validation effectuée avec succès.' : 'Réactivation effectuée avec succès.');
                     window.location.reload();
                 }
                 else {
-                    throw new Error();
+                    isFetching = false;
+                    let data = await response.json();
+                    throw new Error(data.error);
                 }
             } catch (error) {
-                console.error(error);
-                alert('Erreur lors de la validation.');
-                window.location.reload();
+                let flash = `<div class="alert alert-danger alert-dismissible mt-4">
+                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                    ${error.message}
+                    </div>`
+                document.querySelector('.flash').innerHTML = flash;
+                document.querySelector('.flash').querySelector('.btn-close').addEventListener('click', function () {
+                    document.querySelector('.flash').innerHTML = '';
+                });
+                acceptButtons.forEach(button => selectId === '' ? button.disabled = true : button.disabled = false);
+                rejectButtons.forEach(button => button.disabled = false);
+                event.target.innerHTML = 'Accepter';
+            }
+        });
+    });
+
+    rejectButtons.forEach(button => {
+        button.addEventListener('click', async function (event) {
+            let notificationId = event.target.dataset.notificationId;
+            let notificationType = event.target.dataset.notificationType;
+            let selectId = event.target.parentElement.parentElement.querySelector('select').value;
+            let csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+            if (!['Inscription', 'Reactivation'].includes(notificationType)) {
+                alert('Erreur lors du rejet.');
+                return window.location.reload();
+            }
+
+            try {
+                isFetching = true;
+                acceptButtons.forEach(button => button.disabled = true);
+                rejectButtons.forEach(button => button.disabled = true);
+                event.target.innerHTML = 'Rejet en cours...';
+                let response = await sendRequest(`/notifications/${notificationId}/reject`, 'POST', csrfToken);
+            
+                if (response.status === 200) {
+                    isFetching = false;
+                    alert(notificationType === 'Inscription' ? 'Rejet effectué avec succès.' : 'Désactivation effectuée avec succès.');
+                    window.location.reload();
+                }
+                else {
+                    isFetching = false;
+                    let data = await response.json();
+                    throw new Error(data.error);
+                }
+            } catch (error) {
+                let flash = `<div class="alert alert-danger alert-dismissible mt-4">
+                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                    ${error.message}
+                    </div>`
+                document.querySelector('.flash').innerHTML = flash;
+                document.querySelector('.flash').querySelector('.btn-close').addEventListener('click', function () {
+                    document.querySelector('.flash').innerHTML = '';
+                });
+                acceptButtons.forEach(button => selectId === '' ? button.disabled = true : button.disabled = false);
+                rejectButtons.forEach(button => button.disabled = false);
+                event.target.innerHTML = 'Rejeter';
             }
         });
     });
