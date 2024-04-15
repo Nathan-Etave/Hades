5638edff475005e51118163d87a98a21d46f2d3e
Nathan Etave <nathan.etave@gmail.com>
2024-04-15 12:02:33 +0200

    feat/ Ajout du FileReader
    Co-authored-by: RomainMechain <RomainMechain@users.noreply.github.com>

--- app/utils.py
+++ app/utils.py
@@ -12,6 +12,16 @@ from spacy.lang.en.stop_words import STOP_WORDS as en_stop
 from spacy.util import compile_infix_regex
 from spacy import load
 from unidecode import unidecode
+from flask import current_app
+from pandas import read_excel, read_csv
+from docx import Document
+from PIL import Image, UnidentifiedImageError
+import pytesseract
+import fitz
+from xlrd import open_workbook
+from pptx import Presentation
+from pdf2image import convert_from_path
+import os
 
 class SingletonMeta(type):
     """Singleton metaclass.
@@ -31,7 +41,7 @@ class SingletonMeta(type):
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
         return cls._instances[cls]
-    
+
 
 class Whoosh(metaclass=SingletonMeta):
     def __init__(self):
@@ -55,12 +65,12 @@ class Whoosh(metaclass=SingletonMeta):
         conditions = [condition.split('&') for condition in or_conditions]
         subquery = And([Term("path", path.strip().replace(" ", "")), Or([And([Or([Term("content", condition.strip().replace(" ", "")), Term("tags", condition.strip().replace(" ", ""))]) for condition in condition_list]) for condition_list in conditions])])
         return self.searcher.search(subquery)
-    
+
 
 class NLPProcessor(metaclass=SingletonMeta):
     def __init__(self, batch_size=100000):
         self.batch_size = batch_size
-        self.tokenizer_nlp = load("model")
+        self.tokenizer_nlp = load(f"{current_app.root_path}/storage/model")
         self.lemmatizer_nlp = load("fr_dep_news_trf")
         self.tokenizer_nlp.max_length = batch_size
         self.lemmatizer_nlp.max_length = batch_size
@@ -114,3 +124,90 @@ class NLPProcessor(metaclass=SingletonMeta):
 
     def process_tokenize_batch(self, batch):
         return Counter(self.clean(batch))
+
+
+class FileReader(metaclass=SingletonMeta):
+    def __init__(self):
+        self.readers = {
+            "csv": self.read_csv,
+            #"doc": self.read_doc,
+            "docx": self.read_docx,
+            #"json": self.read_json,
+            #"html": self.read_html,
+            #"htm": self.read_html,
+            #"odt": self.read_odt,
+            "pdf": self.read_pdf,
+            "txt": self.read_txt,
+            "xlsx": self.read_xlsx,
+            "xls": self.read_xls,
+            "pptx": self.read_pptx,
+            #"ppt": self.read_ppt,
+            #"gif": self.read_ocr,
+            "jpg": self.read_ocr,
+            "jpeg": self.read_ocr,
+            "png": self.read_ocr,
+            "tiff": self.read_ocr,
+            "tif": self.read_ocr,
+        }
+
+    def read(self, file_path, extension):
+        extension = extension.lower()
+        return self.readers.get(extension, lambda x: None)(file_path)
+    
+    def read_csv(self, file_path):
+        if os.stat(file_path).st_size == 0:
+            return None
+        return read_csv(file_path).to_string(index=False)
+
+    def read_docx(self, file_path):
+        text = ""
+        for paragraph in Document(file_path).paragraphs:
+            text += paragraph.text
+        return text
+    
+    def read_ocr(self, file_path):
+        try:
+            return pytesseract.image_to_string(Image.open(file_path), lang="fra")
+        except UnidentifiedImageError:
+            return None
+
+    def read_pdf(self, file_path):
+        file = fitz.open(file_path)
+        text = ""
+        for page in file:
+            text += page.get_text()
+            text.replace("\n", " ")
+        if text == "":
+            images = convert_from_path(file_path)
+            for image in images:
+                text += pytesseract.image_to_string(image, lang="fra")
+        return text
+
+    def read_txt(self, file_path):
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
+
+    def read_xlsx(self, file_path):
+        df = read_excel(file_path, engine="openpyxl")
+        text = ''
+        for column in df.columns:
+            text += df[column].astype(str).str.cat(sep=' ') + ' '
+            text = text.replace('nan', '')
+        return text
+
+    def read_xls(self, file_path):
+        file = open_workbook(file_path)
+        text = ""
+        for sheet in file.sheets():
+            for row in range(sheet.nrows):
+                for cell in sheet.row(row):
+                    text += str(cell.value) + " "
+        return text
+
+    def read_pptx(self, file_path):
+        text = ""
+        for slide in Presentation(file_path).slides:
+            for shape in slide.shapes:
+                if hasattr(shape, "text"):
+                    text += shape.text
+        return text
