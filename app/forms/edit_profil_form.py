dbd2643444312e03adfa713cceca6a3f5a97647d
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-04-15 17:01:46 +0200

    Feat/ Mise en place de la vérification du mot de passe

---
+++ app/forms/edit_profil_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, EmailField, StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class Edit_profil_form(FlaskForm) :
+
+    last_name = StringField("Nom", validators=[DataRequired()])
+    first_name = StringField("Prénom", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    telephone = StringField("Téléphone", validators=[DataRequired()])
+    password = PasswordField("Mot de passe", validators=[DataRequired()])
+    submit = SubmitField("Modifier")
--- app/profil/routes.py
+++ app/profil/routes.py
@@ -1,17 +1,24 @@
 """Routes for the profil blueprint."""
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from flask_login import login_required, current_user
 from app.profil import bp
 from app.extensions import db
-from app.decorators import admin_required
+from app.forms.edit_profil_form import Edit_profil_form
+from flask_bcrypt import check_password_hash
 
-@bp.route('/', methods=['GET'])
+@bp.route('/', methods=['GET', 'POST'])
 @login_required
-@admin_required
 def profil():
     """Route to display the profile page.
 
     Returns:
         render_template: The profile page.
     """
-    return render_template('profil/index.html', is_authenticated=True, is_admin=current_user.id_Role == 1, user=current_user)
+    form = Edit_profil_form()
+    return render_template('profil/index.html', is_authenticated=True, is_admin=current_user.id_Role == 1, user=current_user, form=form)
+
+@bp.route('/verification', methods=['POST'])
+@login_required
+def verification() :
+    password = request.json.get("password")
+    return jsonify({'verif' : check_password_hash(current_user.mdp_Utilisateur, password)})
