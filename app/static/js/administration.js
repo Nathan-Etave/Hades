e07a3211986090114716ccaa92020b32b3f3a43f
Nathan Etave <nathan.etave@gmail.com>
2024-04-18 17:05:06 +0200

    feat/ Implémentation de la prévisualisation (pdf, img, spreadsheet, ...)

--- app/static/js/administration.js
+++ app/static/js/administration.js
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let file = document.querySelector(`#file-${data.fileId}`);
         file.remove();
     });
-    
+
     socket.on('file_deletion_failed', function (data) {
         alert('La suppression du fichier a échoué');
     });
@@ -110,6 +110,7 @@ document.addEventListener('DOMContentLoaded', function () {
         searchBars.push(folder.querySelector('#fileSearch'));
         intialFiles[folder.dataset.folder] = folder.querySelector(`#filesAccordion${folder.dataset.folder}`).children;
     });
+
     searchBars.forEach((searchBar) => {
         searchBar.addEventListener('input', function (event) {
             if (event.target.value === '') {
@@ -125,9 +126,99 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
     socket.on('search_results', function (data) {
         data.forEach((file) => {
             document.querySelector(`#file-${file.id}`).style.display = 'block';
         });
     });
+
+    const previewModal = new bootstrap.Modal(document.querySelector('#previewModal'));
+    const files = document.querySelectorAll('#file');
+    files.forEach((file) => {
+        file.addEventListener('click', async function (event) {
+            target = event.target;
+            if (event.target.nodeName === 'P' || event.target.nodeName === 'I') {
+                target = event.target.parentElement.parentElement;
+            }
+            const fileId = target.dataset.file;
+            const folderId = target.dataset.folder;
+            const fileType = target.dataset.type;
+            let previewModalLabel = document.querySelector('#previewModalLabel');
+            previewModalLabel.innerHTML = target.innerHTML;
+            const modalBody = document.querySelector('#previewModal').querySelector('.modal-body');
+            modalBody.innerHTML = '';
+
+            switch (fileType) {
+                case 'pdf':
+                case 'jpg':
+                case 'jpeg':
+                case 'png':
+                case 'gif':
+                case 'svg':
+                    const element = iframeElement(fileId, folderId);
+                    modalBody.appendChild(element);
+                    break;
+                case 'xlsx':
+                case 'xls':
+                case 'csv':
+                case 'ods':
+                case 'txt':
+                    const [tabs, content] = await spreadsheetElement(fileId, folderId);
+                    modalBody.appendChild(tabs);
+                    modalBody.appendChild(content);
+                    break;
+                default:
+                    element = defaultElement();
+                    break;
+            }
+            previewModal.show();
+        });
+    });
+
+    function iframeElement(fileId, folderId) {
+        const iframe = document.createElement('iframe');
+        iframe.src = `/dossier/${folderId}/fichier/${fileId}`;
+        iframe.width = '100%';
+        iframe.height = '100%';
+        return iframe;
+    }
+
+    function defaultElement() {
+        const p = document.createElement('p');
+        p.innerHTML = 'Aucune prévisualisation disponible.';
+        return p;
+    }
+
+    async function spreadsheetElement(fileId, folderId) {
+        const url = `/dossier/${folderId}/fichier/${fileId}`;
+        const data = await (await fetch(url)).arrayBuffer();
+        const workbook = XLSX.read(new Uint8Array(data), {type: 'array'});
+    
+        const div = document.createElement('div');
+        div.className = 'tab-content';
+    
+        const ul = document.createElement('ul');
+        ul.className = 'nav nav-tabs';
+    
+        workbook.SheetNames.forEach((sheetName, index) => {
+            const li = document.createElement('li');
+            li.className = 'nav-item';
+            const a = document.createElement('a');
+            a.className = 'nav-link' + (index === 0 ? ' active' : '');
+            a.id = `tab-${index}`;
+            a.setAttribute('data-bs-toggle', 'tab');
+            a.setAttribute('href', `#content-${index}`);
+            a.textContent = sheetName;
+            li.appendChild(a);
+            ul.appendChild(li);
+    
+            const divTab = document.createElement('div');
+            divTab.className = 'tab-pane fade' + (index === 0 ? ' show active' : '') + ' spreadsheet-table';
+            divTab.id = `content-${index}`;
+            divTab.innerHTML = XLSX.utils.sheet_to_html(workbook.Sheets[sheetName]);
+            div.appendChild(divTab);
+        });
+        return [ul, div];
+    }
 });
