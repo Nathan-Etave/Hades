2e405d9629b1e37e1b0facb2f15e6004869f7325
Nathan-Etave <nathan.etave@gmail.com>
2024-04-24 16:01:00 +0200

    feat/ Implémentation du laptop sur le bureau

--- app/static/js/3d_desktop.js
+++ app/static/js/3d_desktop.js
@@ -1,11 +1,13 @@
 /*
 Computer Desk by Zsky [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/OhXey2fljr)
+Laptop by Poly by Google [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/csEfSvMgyOw)
 */
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as CANNON from 'cannon-es';
+import { TWEEN } from 'https://unpkg.com/three@0.139.0/examples/jsm/libs/tween.module.min.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -50,6 +52,7 @@ const gltfLoader = new GLTFLoader();
 gltfLoader.load('/model/desk.glb', (gltf) => {
     gltf.scene.traverse((node) => {
         if (node.isMesh) {
+            node.name = 'desk';
             node.castShadow = true;
             node.receiveShadow = true;
         }
@@ -67,6 +70,28 @@ gltfLoader.load('/model/desk.glb', (gltf) => {
     world.addBody(deskBody);
 });
 
+gltfLoader.load('/model/laptop.glb', (gltf) => {
+    gltf.scene.traverse((node) => {
+        if (node.isMesh) {
+            node.name = 'laptop';
+            node.castShadow = true;
+            node.receiveShadow = true;
+        }
+    });
+    gltf.scene.scale.set(0.003, 0.003, 0.003);
+    gltf.scene.rotation.x = -Math.PI / 2;
+    gltf.scene.rotation.z = -Math.PI / 3;
+    gltf.scene.position.set(-0.85, 0.39, -0.15);
+    scene.add(gltf.scene);
+});
+
+const iframe = document.createElement('iframe');
+iframe.style.position = 'absolute';
+iframe.style.border = 'none';
+iframe.style.display = 'none';
+iframe.src = '/';
+document.body.appendChild(iframe);
+
 const textureLoader = new THREE.TextureLoader();
 const groundTexture = textureLoader.load('/texture/concrete.png');
 groundTexture.wrapS = THREE.RepeatWrapping;
@@ -76,6 +101,7 @@ groundTexture.repeat.set(10, 10);
 const groundGeometry = new THREE.CircleGeometry(5, 32);
 const groundMaterial = new THREE.MeshStandardMaterial({ map: groundTexture });
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.name = 'ground';
 groundMesh.rotation.x = -Math.PI / 2;
 groundMesh.position.y = -0.3;
 groundMesh.receiveShadow = true;
@@ -87,14 +113,108 @@ groundBody.addShape(groundShape);
 groundBody.position.y = -0.3;
 world.addBody(groundBody);
 
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+let lastCameraPosition = camera.position.clone();
+let isLaptopOpen = false;
+function onMouseDown(event) {
+    if (isLaptopOpen) {
+        closeLaptop();
+        return;
+    }
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+    const objects = [];
+    scene.traverse((object) => {
+        if (object.isMesh) {
+            objects.push(object);
+        }
+    });
+    const intersects = raycaster.intersectObjects(objects, true);
+    for (let i = 0; i < intersects.length; i++) {
+        if (intersects[i].object.name === 'laptop') {
+            openLaptop();
+            return;
+        }
+    }
+}
+
+function getLaptopScreenSize() {
+    let width, height;
+    scene.traverse((object) => {
+        if (object.isMesh && object.name === 'laptop') {
+            if (object.material.name === 'blinn11SG') {
+                let geometry = object.geometry;
+                geometry.computeBoundingBox();
+
+                let min = geometry.boundingBox.min.clone();
+                let max = geometry.boundingBox.max.clone();
+
+                object.updateMatrixWorld(true);
+                min.applyMatrix4(object.matrixWorld);
+                max.applyMatrix4(object.matrixWorld);
+
+                min.project(camera);
+                max.project(camera);
+
+                width = Math.abs(max.x - min.x) * window.innerWidth / 1.95;
+                height = Math.abs(max.y - min.y) * window.innerHeight / 1.95;
+            }
+        }
+    });
+    return { width, height };
+}
+
+function openLaptop() {
+    lastCameraPosition = camera.position.clone();
+    const targetPosition = { x: -0.809, y: 0.542, z: -0.066 };
+    const targetRotation = { x: -0.058, y: 0.505, z: 0.03 };
+    controls.enabled = false;
+    new TWEEN.Tween(camera.position)
+        .to(targetPosition, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+
+    new TWEEN.Tween(camera.rotation)
+        .to(targetRotation, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onComplete(() => {
+            iframe.style.display = 'block';
+            let { width, height } = getLaptopScreenSize();
+            iframe.style.width = `${width}px`;
+            iframe.style.height = `${height}px`;
+            iframe.style.top = `${window.innerHeight / 2 - height / 2}px`;
+            iframe.style.left = `${window.innerWidth / 2 - width / 2}px`;
+            isLaptopOpen = true;
+        })
+        .start();
+}
+
+function closeLaptop() {
+    iframe.style.display = 'none';
+    controls.enabled = true;
+    isLaptopOpen = false;
+    new TWEEN.Tween(camera.position)
+        .to(lastCameraPosition, 2000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+}
+
+window.addEventListener('mousedown', onMouseDown);
+
 function updatePhysics() {
     world.step(1 / 60);
 }
 
-function animate() {
+function animate(time) {
     requestAnimationFrame(animate);
-    controls.update();
+    if (controls.enabled) {
+        controls.update();
+    }
     updatePhysics();
+    TWEEN.update(time);
     renderer.render(scene, camera);
 }
 animate();
