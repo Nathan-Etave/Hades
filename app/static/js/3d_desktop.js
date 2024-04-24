85f621d043dfc75ca048d64c25ec5913a20237ab
Nathan-Etave <nathan.etave@gmail.com>
2024-04-24 22:24:16 +0200

    feat/ Ajout des fichiers sur le bureau

--- app/static/js/3d_desktop.js
+++ app/static/js/3d_desktop.js
@@ -7,6 +7,7 @@ Debris Papers by Quaternius (https://poly.pizza/m/MujITy1NRR)
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import * as CANNON from 'cannon-es';
 import { TWEEN } from 'https://unpkg.com/three@0.139.0/examples/jsm/libs/tween.module.min.js';
 
@@ -69,6 +70,16 @@ gltfLoader.load('/model/desk.glb', (gltf) => {
     deskBody.addShape(deskShape);
     deskBody.position.set(center.x, center.y, center.z);
     world.addBody(deskBody);
+
+    const topDesk = new THREE.Mesh(new THREE.BoxGeometry(size.x, 0, size.z), new THREE.MeshStandardMaterial({ transparent: true, opacity: 0.0 }));
+    topDesk.position.set(center.x, center.y + size.y / 2, center.z);
+    topDesk.name = 'topDesk';
+    scene.add(topDesk);
+
+    const borderDesk = new THREE.Mesh(new THREE.BoxGeometry(size.x, size.y, size.z), new THREE.MeshStandardMaterial({ transparent: true, opacity: 0.0 }));
+    borderDesk.position.set(center.x, center.y - 0.001, center.z);
+    borderDesk.name = 'borderDesk';
+    scene.add(borderDesk);
 });
 
 gltfLoader.load('/model/laptop.glb', (gltf) => {
@@ -141,9 +152,12 @@ const groundBody = new CANNON.Body({ mass: 0 });
 groundBody.addShape(groundShape);
 groundBody.position.y = -0.3;
 world.addBody(groundBody);
+groundMesh.body = groundBody;
+groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
+let selectedObject = null;
 
 let lastCameraPosition = camera.position.clone();
 let isLaptopOpen = false;
@@ -165,7 +179,52 @@ function onMouseDown(event) {
     for (let i = 0; i < intersects.length; i++) {
         if (intersects[i].object.name === 'laptop') {
             openLaptop();
-            return;
+            break;
+        }
+        else if (intersects[i].object.name === 'file') {
+            selectedObject = intersects[i].object;
+            selectedObject.body.mass = 0;
+            selectedObject.body.updateMassProperties();
+            controls.enabled = false;
+            break;
+        }
+    }
+}
+
+function onMouseUp() {
+    if (selectedObject) {
+        selectedObject.body.mass = 1;
+        selectedObject.body.updateMassProperties();
+        selectedObject = null;
+        controls.enabled = true;
+    }
+}
+
+function onMouseMove(event) {
+    if (selectedObject) {
+        selectedObject.body.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        raycaster.setFromCamera(mouse, camera);
+        let topDesk = scene.getObjectByName('topDesk');
+        const intersectsTopDesk = raycaster.intersectObject(topDesk);
+        let borderDesk = scene.getObjectByName('borderDesk');
+        const intersectsBorderDesk = raycaster.intersectObject(borderDesk);
+        let ground = scene.getObjectByName('ground');
+        const intersectsGround = raycaster.intersectObject(ground);
+        if (intersectsTopDesk.length > 0 || intersectsGround.length > 0) {
+            selectedObject.body.angularVelocity.set(0, 0, 0);
+            selectedObject.body.velocity.set(0, 0, 0);
+            selectedObject.body.angularVelocity.set(0, 0, 0);
+            if (intersectsTopDesk.length > 0) {
+                selectedObject.body.position.copy(intersectsTopDesk[0].point);
+            }
+            else {
+                if (intersectsBorderDesk.length > 0) {
+                    return;
+                }
+                selectedObject.body.position.copy(intersectsGround[0].point);
+            }
         }
     }
 }
@@ -224,17 +283,99 @@ function openLaptop() {
 function closeLaptop() {
     iframe.style.display = 'none';
     controls.enabled = true;
-    isLaptopOpen = false;
     new TWEEN.Tween(camera.position)
-        .to(lastCameraPosition, 2000)
+        .to(lastCameraPosition, 1000)
         .easing(TWEEN.Easing.Quadratic.Out)
+        .onComplete(() => {
+            isLaptopOpen = false;
+        })
         .start();
 }
 
 window.addEventListener('mousedown', onMouseDown);
+window.addEventListener('mouseup', onMouseUp);
+window.addEventListener('mousemove', onMouseMove);
+
+const socket = io.connect('/file_handler');
+
+let desktopFiles = JSON.parse(localStorage.getItem('desktop'));
+if (desktopFiles === null) {
+    desktopFiles = [];
+    localStorage.setItem('desktop', JSON.stringify(desktopFiles));
+}
+socket.emit('get_files_details', { files: desktopFiles });
+
+socket.on('files_details', (data) => {
+    data.forEach((file) => {
+        createFileObject(file);
+    });
+});
+
+const fontLoader = new FontLoader();
+let font = null;
+fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', (response) => {
+    font = response;
+});
+function createFileObject(file) {
+    const fileGeometry = new THREE.BoxGeometry(0.15, 0.2, 0.025);
+    const fileMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const fileMesh = new THREE.Mesh(fileGeometry, fileMaterial);
+    fileMesh.position.set(Math.random() * 0.2 - 0.1, 1, Math.random() * 0.2 - 0.1);
+    fileMesh.receiveShadow = true;
+    fileMesh.castShadow = true;
+    fileMesh.name = 'file';
+    scene.add(fileMesh);
+
+    const shapes = font.generateShapes(truncateString(file.nom_Fichier, file.nom_Fichier.length % 5, 4), 0.025);
+    const geometry = new THREE.ShapeGeometry(shapes);
+    geometry.computeBoundingBox();
+
+    const xMid = - 0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
+    geometry.translate(xMid, 0.06, 0);
+
+    const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+    const textMesh = new THREE.Mesh(geometry, textMaterial);
+    textMesh.position.z = 0.013;
+    fileMesh.add(textMesh);
+
+    const fileShape = new CANNON.Box(new CANNON.Vec3(0.075, 0.1, 0.0125));
+    const fileBody = new CANNON.Body({ mass: 1 });
+    fileBody.addShape(fileShape);
+    fileBody.position.set(fileMesh.position.x, fileMesh.position.y, fileMesh.position.z);
+    fileBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+    world.addBody(fileBody);
+
+    fileMesh.body = fileBody;
+}
+
+function truncateString(str, num, lineLength) {
+    if (str.length > num) {
+        let truncated = '';
+        let lineCount = 0;
+        let exceededLineLimit = false;
+        for (let i = 0; i < str.length; i += lineLength) {
+            if (lineCount < lineLength) {
+                truncated += str.slice(i, i + lineLength) + '\n';
+                lineCount++;
+            } else {
+                exceededLineLimit = true;
+                break;
+            }
+        }
+        return exceededLineLimit ? truncated.slice(0, truncated.length - 1) + '...' : truncated.slice(0, truncated.length - 1);
+    } else {
+        return str;
+    }
+}
 
 function updatePhysics() {
     world.step(1 / 60);
+    scene.traverse(function (object) {
+        if (object.isMesh && object.body) {
+            object.position.copy(object.body.position);
+            object.quaternion.copy(object.body.quaternion);
+        }
+    });
 }
 
 function animate(time) {
