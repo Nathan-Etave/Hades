1c764b9ea75a061087722780e7505c9e94d089a7
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-04-25 12:53:35 +0200

    Feat/ Implémentation des boutons dans le bureau

--- app/static/js/desktop.js
+++ app/static/js/desktop.js
@@ -1,4 +1,5 @@
 import { previewAfterRender } from './preview.js';
+import { baseAfterRender } from './base.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     let deskList = JSON.parse(localStorage.getItem('desktop'));
@@ -6,15 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
         deskList = [];
         localStorage.setItem('desktop', JSON.stringify(deskList));
     }
+    let csrfToken = document.querySelector('meta[name="csrf-token"]').content;
 
     const socket = io.connect('/file_handler');
-
     socket.emit('get_files_details', { 'files': deskList });
 
     let currentFile = 1;
+    let desktop = document.getElementById('desk-section');
 
     socket.on('files_details', function (data) {
-        let desktop = document.getElementById('desk-section');
         let modalFooter = document.querySelector('.nav-tabs');
         desktop.innerHTML = '';
 
@@ -22,6 +23,8 @@ document.addEventListener('DOMContentLoaded', function () {
         data.forEach(function (file) {
             let div = document.createElement('div');
             div.className = 'col-2';
+            div.id = "div-file-" + file.id_Fichier;
+            div.setAttribute('data-number', deskFileNumber);
 
             let card = document.createElement('div');
             card.className = 'card';
@@ -43,6 +46,15 @@ document.addEventListener('DOMContentLoaded', function () {
                                  </div>
                                  <div>
                                     <p class="card-title h5" style="font-size: 1.5em; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;">${file.nom_Fichier}<p>
+                                 </div>
+                                 <div class="desktop-element">
+                                    <a href="#" id="${ file.id_Fichier }" class="favori" is-fav="${ file.is_favorite }" onclick="event.stopPropagation();">
+                                        <i class="fa-solid fa-star fa-lg me-2" style="color: #FFD43B;"
+                                            id="fav-${ file.id_Fichier }"></i>
+                                    </a>
+                                    <a href="#" id="${ file.id_Fichier }" class="desktop-btn" onclick="event.stopPropagation();">
+                                        <i class="fa-regular fa-square-minus fa-lg" id="desk-${ file.id_Fichier }"></i>
+                                    </a>
                                  </div>`;
 
             fileDiv.addEventListener('click', function () {
@@ -86,6 +98,78 @@ document.addEventListener('DOMContentLoaded', function () {
             deskFileNumber++;
 
         });
+
+        let favs = document.querySelectorAll('.favori');
+            favs.forEach(function (fav) {
+                let id = fav.id;
+                let isFav = fav.getAttribute('is-fav');
+                let etoile = document.getElementById("fav-" + id);
+                if (isFav === "true") {
+                    fav.className = "favori-true";
+                }
+                else {
+                    fav.className = "favori-false";
+                    etoile.className = "fa-regular fa-star fa-lg me-2";
+                }
+                fav.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    if (fav.className === "favori-true") {
+                        fetch("/favori/" + id, {
+                            method: "DELETE",
+                            headers: {
+                                "Content-Type": "application/json",
+                                'X-CSRFToken': csrfToken
+                            }
+                        })
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data.status === "ok") {
+                                    fav.className = "favori-false";
+                                    etoile.className = "fa-regular fa-star fa-lg me-2";
+                                }
+                                else {
+                                    alert("Erreur lors de la suppression du favori");
+                                }
+                            });
+                    }
+                    else {
+                        fetch("/recherche/favori/" + id, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                'X-CSRFToken': csrfToken
+                            }
+                        })
+                            .then(response => response.json())
+                            .then(data => {
+                                if (data.status === "ok") {
+                                    fav.className = "favori-true";
+                                    etoile.className = "fa-solid fa-star fa-lg me-2";
+                                }
+                                else {
+                                    alert("Erreur lors de l'ajout du favori");
+                                }
+                            });
+                    }
+                });
+            });
+
+
+        let deskBtns = document.querySelectorAll('.desktop-btn');
+        deskBtns.forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                let id = btn.id;
+                let deskList = JSON.parse(localStorage.getItem('desktop'));
+                let index = deskList.indexOf(id);
+                if (index !== -1) {
+                deskList.splice(index, 1);
+                }
+                localStorage.setItem('desktop', JSON.stringify(deskList));
+                window.location.reload();
+            });
+        });
+
+
         previewAfterRender();
 
     });
@@ -117,6 +201,13 @@ document.addEventListener('DOMContentLoaded', function () {
         let nav = document.querySelector('.nav-' + currentFile);
         nav.classList.remove('active');
         currentFile = 0;
+    });
+
+    let btnClear = document.getElementById("clear");
+    btnClear.addEventListener('click', function () {
+        localStorage.setItem('desktop', JSON.stringify([]));
+        desktop.innerHTML = '';
+        baseAfterRender(0);
     });
 
 });
