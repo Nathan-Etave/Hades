61eea741aa3770e67b6d1aec4046292b2f2e88fb
Nathan Etave <nathan.etave@gmail.com>
2024-05-07 09:23:44 +0200

    feat/ Mise à jour du nom de l'application

--- app/mail/mail.py
+++ app/mail/mail.py
@@ -19,7 +19,7 @@ def send_email(subject, sender, recipients, html_body):
 
 def send_registration_request_email(email):
     subject = "Demande d'inscription en attente de validation"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = "<p>Bonjour, votre demande d'inscription est en attente de validation. Vous recevrez un email de confirmation dès que votre compte sera activé.</p>"
     send_email(subject, sender, recipients, html_body)
@@ -27,7 +27,7 @@ def send_registration_request_email(email):
 
 def send_registration_confirmation_email(email, password):
     subject = "Inscription validée"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = f"<p>Bonjour, votre demande d'inscription a été validée. Vous pouvez désormais vous connecter à la plateforme.</p><p>Votre mot de passe est: {password}</p>"
     send_email(subject, sender, recipients, html_body)
@@ -35,7 +35,7 @@ def send_registration_confirmation_email(email, password):
 
 def send_registration_rejection_email(email):
     subject = "Demande d'inscription refusée"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = "<p>Bonjour, votre demande d'inscription a été refusée.</p>"
     send_email(subject, sender, recipients, html_body)
@@ -43,7 +43,7 @@ def send_registration_rejection_email(email):
 
 def send_reactivation_confirmation_email(email):
     subject = "Réactivation de votre compte"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = "<p>Bonjour, votre compte a été réactivé. Vous pouvez désormais vous connecter à la plateforme.</p>"
     send_email(subject, sender, recipients, html_body)
@@ -51,7 +51,7 @@ def send_reactivation_confirmation_email(email):
 
 def send_reactivation_rejection_email(email):
     subject = "Réactivation de votre compte refusée"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = f"<p>Bonjour, la réactivation de votre compte a été refusée.</p>"
     send_email(subject, sender, recipients, html_body)
@@ -69,7 +69,7 @@ def send_forgotten_password_email(email, uuid):
     None
     """
     subject = "Réinitialisation de votre mot de passe"
-    sender = f'NOM_DE_LA_PLATEFORME <{os.environ.get("MAIL_USERNAME")}>'
+    sender = f'Hadès <{os.environ.get("MAIL_USERNAME")}>'
     recipients = email
     html_body = f"<p>Bonjour, une demande de réinitialisation de mot de passe a été faite sur votre compte. \n Pour la valider, cliquez sur le lien suivant :</p> <a href='http://127.0.0.1:5000/connexion/reinitialisation/{uuid}'>Réinitialisation du mot de passe</a>"
     send_email(subject, sender, recipients, html_body)
