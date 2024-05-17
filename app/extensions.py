e20fb467409fee3c3da120eb57d5c2131f86af79
Nathan Etave <nathan.etave@gmail.com>
2024-05-17 15:46:26 +0200

    feat/ Ajout de Flask-Compress pour la compression Gzip

--- app/extensions.py
+++ app/extensions.py
@@ -4,6 +4,7 @@ from flask_redis import FlaskRedis
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_wtf.csrf import CSRFProtect
+from flask_compress import Compress
 from celery import Celery
 
 crsf = CSRFProtect()
@@ -12,3 +13,4 @@ socketio = SocketIO()
 celery = Celery(__name__, broker=Config.CELERY_BROKER_URL, result_backend=Config.RESULT_BACKEND, include=['app.tasks'])
 redis = FlaskRedis()
 db = SQLAlchemy()
+compress = Compress()
